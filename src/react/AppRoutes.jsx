3c0efab72ffcc8a2e6ee27ad01591cde05ce1ae4
Annika Dörner <Annika.Doerner@Student.HTW-Berlin.de>
2025-07-01 16:11:04 +0200

    AppRoutes angepasst

--- src/react/AppRoutes.jsx
+++ src/react/AppRoutes.jsx
@@ -4,29 +4,18 @@ import { Route, Routes } from 'react-router-dom'
 import Welcome from './Pages/Welcome/Welcome'
 import Home from './Pages/Home/Home'
 import Stundenplan from './Pages/Stundenplan/Stundenplan'
-import Auswertung from './Pages/Auswertung/Auswertung'
 import AuswertungStart from './Pages/Auswertung/AuswertungStart'
 import MedTeamBestätigung from './Pages/Auswertung/MedTeamBestätigung'
 
 import Error404 from './Pages/Error404'
-import AppLayout from './AppLayout'
 
 const AppRoutes = () => (
   <Routes>
-    {/* Public Landing Page */}
     <Route path="/" element={<Welcome />} />
-
-    {/* AppLayout für "echte" Appseiten */}
-    <Route path="/" element={<AppLayout />}>
-      <Route path="home" element={<Home />} />
-      <Route path="stundenplan" element={<Stundenplan />} />
-      <Route path="auswertung" element={<Auswertung />}>
-        <Route index element={<AuswertungStart />} />
-        <Route path="medteam" element={<MedTeamBestätigung />} />
-      </Route>
-    </Route>
-
-    {/* Fallback */}
+    <Route path="/home" element={<Home />} />
+    <Route path="/stundenplan" element={<Stundenplan />} />
+    <Route path="/auswertung" element={<AuswertungStart />} />
+    <Route path="/auswertung/medteam" element={<MedTeamBestätigung />} />
     <Route path="*" element={<Error404 />} />
   </Routes>
 )
