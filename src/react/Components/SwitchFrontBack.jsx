aa8578c75ec6a22d854b815e85c6033e4098904d
Isabell Arnke <isabellarnke@outlook.com>
2025-07-05 21:07:10 +0200

    add Switch Bar für Körperansicht Vorne und Hinten

---
+++ src/react/Components/SwitchFrontBack.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { alpha, styled } from '@mui/material/styles'
+import { pink } from '@mui/material/colors'
+import { Switch } from '@mui/material'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
+
+// PinkSwitch Code direkt aus Material UI übernommen
+
+const PinkSwitch = styled(Switch)(({ theme }) => ({
+  '& .MuiSwitch-switchBase.Mui-checked': {
+    color: pink[600],
+    '&:hover': {
+      backgroundColor: alpha(pink[600], theme.palette.action.hoverOpacity),
+    }
+  },
+  '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+    backgroundColor: pink[600]
+  }
+}))
+
+const SwitchFrontBack = ({ checked, onChange }) => (
+  <Stack direction="row" spacing={1} alignItems="center">
+    <Typography>Vorne</Typography>
+    <PinkSwitch
+      checked={checked}
+      onChange={onChange}
+      inputProps={{ 'aria-label': 'Front/Back switch' }}
+    />
+    <Typography>Hinten</Typography>
+  </Stack>
+)
+
+SwitchFrontBack.propTypes = {
+  checked: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired
+}
+
+export default SwitchFrontBack
