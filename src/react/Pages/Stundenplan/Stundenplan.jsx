a157144a075a3b0342c3418d7efebf766dc6cd0e
Isabell Arnke <isabellarnke@outlook.com>
2025-07-20 13:00:18 +0200

    Zurück Button enfernt

--- src/react/Pages/Stundenplan/Stundenplan.jsx
+++ src/react/Pages/Stundenplan/Stundenplan.jsx
@@ -1,32 +1,17 @@
 import React from 'react'
 
-import { useNavigate } from 'react-router-dom'
-
 import { Stack } from '@mui/material'
-import { ArrowBack as BackIcon } from '@mui/icons-material'
-import BackButton from '../../Components/Buttons/BackButton'
 
 import UploadStundenplan from '../../Components/UploadStundenplan'
 
-const Stundenplan = () => {
-  const navigate = useNavigate()
-
-  return (
-    <Stack
-      flex="1 1 auto"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <UploadStundenplan />
-      <BackButton
-        variant="contained"
-        startIcon={<BackIcon />}
-        onClick={() => navigate('/home')}
-      >
-        Zurück
-      </BackButton>
-    </Stack>
-  )
-}
+const Stundenplan = () => (
+  <Stack
+    flex="1 1 auto"
+    justifyContent="center"
+    alignItems="center"
+  >
+    <UploadStundenplan />
+  </Stack>
+)
 
 export default Stundenplan
