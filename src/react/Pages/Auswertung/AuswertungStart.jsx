6b70b3a8b40d76630d3f71693fe2f5e4f79f9290
Annika Dörner <Annika.Doerner@Student.HTW-Berlin.de>
2025-07-20 15:38:53 +0200

    Header und Bottom Buttons sind wieder ganz unten / oben

--- src/react/Pages/Auswertung/AuswertungStart.jsx
+++ src/react/Pages/Auswertung/AuswertungStart.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Grid, Button, Snackbar, Typography } from '@mui/material'
+import { Grid, Button, Snackbar, Typography, Box } from '@mui/material'
 import { Add as AddIcon } from '@mui/icons-material'
 import PrimaryButton from '../../Components/Buttons/PrimaryButton'
 import { getPainEntries, loadDummyData } from '../../../hooks/usePainEntries'
@@ -19,52 +19,48 @@ const AuswertungStart = () => {
     setPainEntries(getPainEntries())
   }, [])
 
-  const handleCloseSnackbar = () => {
-    setShowSnackbar(false)
-  }
-
+  const handleCloseSnackbar = () => setShowSnackbar(false)
   const openScannerDialog = () => {
     setQrDialogKey(prev => prev + 1)
     setOpenQRDialog(true)
   }
-
-  const closeScannerDialog = () => {
-    setOpenQRDialog(false)
-  }
+  const closeScannerDialog = () => setOpenQRDialog(false)
 
   return (
-    <Grid container spacing={2} justifyContent="center" sx={{ px: 2, py: 4 }}>
-      <Grid item xs={12} md={12}>
-        <SchmerzGraph />
-      </Grid>
+    <Box sx={{ flex: '1 1 auto', width: '100%' }}>
+      <Grid container spacing={2} justifyContent="center" sx={{ px: 2, py: 2 }}>
+        <Grid item xs={12}>
+          <SchmerzGraph />
+        </Grid>
 
-      <Grid item xs={12} md={12}>
-        <TagesCountdown daysLeft={daysLeft} />
-      </Grid>
+        <Grid item xs={12}>
+          <TagesCountdown daysLeft={daysLeft} />
+        </Grid>
 
-      <Grid item xs={12} md={6} display="flex" justifyContent="center">
-        <Button
-          variant="outlined"
-          startIcon={<AddIcon />}
-          onClick={() => {
-            setIsLoading(true)
-            setTimeout(() => {
-              loadDummyData()
-              setPainEntries(getPainEntries())
-              setIsLoading(false)
-              setShowSnackbar(true)
-            }, 500)
-          }}
-          disabled={isLoading}
-        >
-          {isLoading ? 'Laden...' : 'Testdaten laden'}
-        </Button>
-      </Grid>
+        <Grid item xs={12} display="flex" justifyContent="center">
+          <Button
+            variant="outlined"
+            startIcon={<AddIcon />}
+            onClick={() => {
+              setIsLoading(true)
+              setTimeout(() => {
+                loadDummyData()
+                setPainEntries(getPainEntries())
+                setIsLoading(false)
+                setShowSnackbar(true)
+              }, 500)
+            }}
+            disabled={isLoading}
+          >
+            {isLoading ? 'Laden...' : 'Testdaten laden'}
+          </Button>
+        </Grid>
 
-      <Grid item xs={12} display="flex" justifyContent="center">
-        <PrimaryButton variant="contained" onClick={openScannerDialog}>
-          Med Team Bestätigen
-        </PrimaryButton>
+        <Grid item xs={12} display="flex" justifyContent="center">
+          <PrimaryButton variant="contained" onClick={openScannerDialog}>
+            Med Team Bestätigen
+          </PrimaryButton>
+        </Grid>
       </Grid>
 
       <QrVerifyDialog
@@ -79,7 +75,7 @@ const AuswertungStart = () => {
         onClose={handleCloseSnackbar}
         message="Testdaten erfolgreich geladen!"
       />
-    </Grid>
+    </Box>
   )
 }
 
