e407285823841faf622a8442c1796177dda763d0
Annika Dörner <Annika.Doerner@Student.HTW-Berlin.de>
2025-07-20 17:13:46 +0200

    Name in Körperansicht raus

--- src/react/Pages/Home/Home.jsx
+++ src/react/Pages/Home/Home.jsx
@@ -1,30 +1,20 @@
 import React from 'react'
-import { Stack, Typography, Container } from '@mui/material'
+import { Stack, Container } from '@mui/material'
 
 import BodyClick from '../../Components/BodyClick'
 
-const Home = () => {
-  const displayName = localStorage.getItem('displayName')
-
-  return (
-    <Stack
-      flex="1 1 auto"
-      justifyContent="center"
-      alignItems="center"
-      spacing={4}
-      sx={{ padding: 2 }}
-    >
-      <Typography variant="h5">
-        Körperansicht von
-        {' '}
-        {displayName}
-      </Typography>
-
-      <Container>
-        <BodyClick />
-      </Container>
-    </Stack>
-  )
-}
+const Home = () => (
+  <Stack
+    flex="1 1 auto"
+    justifyContent="center"
+    alignItems="center"
+    spacing={4}
+    sx={{ padding: 2 }}
+  >
+    <Container>
+      <BodyClick />
+    </Container>
+  </Stack>
+)
 
 export default Home
