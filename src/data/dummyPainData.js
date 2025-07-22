171417daa407e17a02cdf5567938669e106394af
Isabell Arnke <isabellarnke@outlook.com>
2025-07-22 20:47:39 +0200

    Rücken in Unterer Rücken rechts umbenannt

--- src/data/dummyPainData.js
+++ src/data/dummyPainData.js
@@ -2,22 +2,22 @@ const dummyPainData = [
   // 21.07.2025
   { bodyPart: 'Knie links', pain: 3, movements: ['spruenge'], date: '2025-07-21T10:00:00.000Z' },
   { bodyPart: 'Fuß rechts', pain: 6, movements: ['schnell'], date: '2025-07-21T14:00:00.000Z' },
-  { bodyPart: 'Rücken', pain: 2, movements: ['spitze'], date: '2025-07-21T16:00:00.000Z' },
+  { bodyPart: 'Unterer Rücken rechts', pain: 2, movements: ['spitze'], date: '2025-07-21T16:00:00.000Z' },
 
   // 22.07.2025
   { bodyPart: 'Knie links', pain: 5, movements: ['langsam', 'spitze'], date: '2025-07-22T10:00:00.000Z' },
   { bodyPart: 'Fuß rechts', pain: 4, movements: ['spruenge'], date: '2025-07-22T14:00:00.000Z' },
-  { bodyPart: 'Rücken', pain: 7, movements: ['schnell'], date: '2025-07-22T16:00:00.000Z' },
+  { bodyPart: 'Unterer Rücken rechts', pain: 7, movements: ['schnell'], date: '2025-07-22T16:00:00.000Z' },
 
   // 23.07.2025
   { bodyPart: 'Knie links', pain: 7, movements: ['schnell'], date: '2025-07-23T10:00:00.000Z' },
   { bodyPart: 'Fuß rechts', pain: 8, movements: ['spruenge'], date: '2025-07-23T14:00:00.000Z' },
-  { bodyPart: 'Rücken', pain: 3, movements: ['langsam'], date: '2025-07-23T16:00:00.000Z' },
+  { bodyPart: 'Unterer Rücken rechts', pain: 3, movements: ['langsam'], date: '2025-07-23T16:00:00.000Z' },
 
   // 24.07.2025
   { bodyPart: 'Knie links', pain: 6, movements: ['spruenge', 'langsam'], date: '2025-07-24T10:00:00.000Z' },
   { bodyPart: 'Fuß rechts', pain: 2, movements: ['schnell'], date: '2025-07-24T12:00:00.000Z' },
-  { bodyPart: 'Rücken', pain: 6, movements: ['spitze', 'schnell'], date: '2025-07-24T14:00:00.000Z' }
+  { bodyPart: 'Unterer Rücken rechts', pain: 6, movements: ['spitze', 'schnell'], date: '2025-07-24T14:00:00.000Z' }
 ]
 
 export default dummyPainData
